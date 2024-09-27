365a338cbac37d5fe8e49e29519fde3520060e44
Dana <danacastroman@gmail.com>
2024-09-27 17:34:46 -0300

    home second section

---
+++ src/components/home/second-section/StartText.tsx
@@ -0,0 +1,18 @@
+export const StartText = () => {
+  return (
+    <div className="text-center space-y-4 px-4 bg-white sm:bg-transparent shadow-lg sm:shadow-none rounded-lg sm:rounded-none p-6 sm:p-0">
+      <p className="text-lg">
+        ¡Esta aplicación ha cambiado completamente la forma en que gestiono mis
+        campeonatos! Tengo toda la información en un solo lugar, está organizada
+        de manera clara y fácil de seguir.
+      </p>
+      <p className="font-bold text-lg">
+        ¡Definitivamente recomendaría esta app a cualquiera que quiera organizar
+        un torneo, de cualquier disciplina!
+      </p>
+      <button className="bg-black text-white py-2 px-4 hover:bg-gray-800 transition rounded-lg font-semibold mt-4">
+        ¡Quiero comenzar!
+      </button>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,22 @@
 import { HomeDescription } from "../components/home/HomeDescription";
 import { Card } from "../components/home/Card";
+import { PhoneScreens } from "../components/home/second-section/PhoneScreens";
+import { StartText } from "../components/home/second-section/StartText";
 import lock from "../assets/cards_icons/lock.png";
 import network from "../assets/cards_icons/network.png";
+import paint_brush from "../assets/cards_icons/paint-brush.png";
 
 import "./Home.scss";
 
 export const Home = () => {
   return (
-    <div className="cornered-section relative py-20 px-12 overflow-hidden">
-      <HomeDescription />
+    <>
+      <div className="cornered-section relative py-20 px-12 overflow-hidden">
+        <HomeDescription />
         <div className="container mx-auto flex justify-end">
           <div className="home-content flex flex-wrap justify-around items-start gap-8">
             <Card
-              icon={<img className="paint-brush" src={lock}></img>}
+              icon={<img className="paint-brush" src={paint_brush}></img>}
               title="Customizable"
               description="Puedes personalizar nuestra aplicación para que se adapte perfectamente a tu torneo u organización. Elige los colores, logotipos y más para reflejar tu identidad única. ¡Haz que tu experiencia de torneo sea más fácil y efectiva que nunca!"
             />
@@ -28,6 +32,11 @@ export const Home = () => {
             />
           </div>
         </div>
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
+          <PhoneScreens />
+          <StartText />
+        </div>
       </div>
+    </>
   );
 };
